9f6228896d9cf48fa225c41e2b59077ac5c326a8
donchodonev <dummyacc9292@gmail.com>
2021-06-11 15:25:40 +0300

    added another exercise solution added Puppy class

--- 1.Inheritance/1.Lab/Farm/Farm/StartUp.cs
+++ 1.Inheritance/1.Lab/Farm/Farm/StartUp.cs
@@ -6,10 +6,10 @@ namespace Farm
     {
         public static void Main(string[] args)
         {
-            Dog dog = new Dog();
-            dog.Bark();
-            dog.Bark();
-
+            Puppy puppy = new Puppy();
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
         }
     }
 }
