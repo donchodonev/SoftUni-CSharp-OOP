25e8d495c2fbb7e3b16ef13c2d9392871f3c9bc7
donchodonev <dummyacc9292@gmail.com>
2021-06-11 16:16:17 +0300

    added another exercise solution

---
+++ 1.Inheritance/1.Lab/Farm/CustomRandomList/RandomList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private readonly Random random;
+        public RandomList()
+        {
+            random = new Random();
+        }
+        public string RandomString()
+        {
+            if (this.Count == 0)
+            {
+                return "";
+            }
+            int index = random.Next(0, this.Count);
+
+            string stringToReturn = this[index];
+
+            this.RemoveAt(index);
+
+            return stringToReturn;
+        }
+    }
+}
---
+++ 1.Inheritance/1.Lab/Farm/CustomRandomList/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            RandomList rl = new RandomList
+            {
+                "string1",
+                "string2",
+                "string3",
+                "string4",
+                "string5"
+            };
+
+            Console.WriteLine(rl.RandomString());
+        }
+    }
+}
