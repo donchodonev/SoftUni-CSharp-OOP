aca837eca10b579d507554102e4518f9a6c54286
donchodonev <dummyacc9292@gmail.com>
2021-06-11 16:40:58 +0300

    added final exercise solution

---
+++ 1.Inheritance/1.Lab/Farm/CustomStack/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
