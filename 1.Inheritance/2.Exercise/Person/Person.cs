c6f91cdbbf1fe17a5eaa32adf955a34da9341502
donchodonev <dummyacc9292@gmail.com>
2021-06-14 10:03:31 +0300

    added exercise solutions folder added .docx exercise tasks file added first exercise solution

---
+++ 1.Inheritance/2.Exercise/Person/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // 2. Add Constructor
+
+
+        // 4. Add Methods
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}",
+                                 Name,
+                                 Age));
+
+            return stringBuilder.ToString();
+        }
+    }
+}
