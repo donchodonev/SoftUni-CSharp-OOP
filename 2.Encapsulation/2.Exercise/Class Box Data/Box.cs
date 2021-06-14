96b4b1c88e87ace98f6604ab8416066d0e5ed51c
donchodonev <dummyacc9292@gmail.com>
2021-06-14 18:18:11 +0300

    added first exercise solution added exercise solution separately archived

---
+++ 2.Encapsulation/2.Exercise/Class Box Data/Box.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        private double Length
+        {
+            get => length;
+            set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+                length = value;
+            }
+        }
+        private double Width
+        {
+            get => width;
+            set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                width = value;
+            }
+        }
+        private double Height
+        {
+            get => height;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+                height = value;
+            }
+        }
+        public  void SurfaceArea()
+        {
+            double surface = 
+                2 * (Length * Width)
+                + 
+                2 * (Length * Height) 
+                + 
+                2 * (Width * Height) ;
+
+            Console.WriteLine($"Surface Area - {surface:F2}");
+        }
+        public void LateralSurfaceArea()
+        {
+            double lateralSurface =
+                2 * (Length * Height)
+                +
+                2 * (Width * Height);
+
+            Console.WriteLine($"Lateral Surface Area - {lateralSurface:F2}");
+        }
+        public void Volume()
+        {
+            double volume = Length * Width * Height;
+            Console.WriteLine($"Volume - {volume:F2}");
+        }
+    }
+}
---
+++ 2.Encapsulation/2.Exercise/Class Box Data/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box mybox = new Box(length, width, height);
+
+                mybox.SurfaceArea();
+                mybox.LateralSurfaceArea();
+                mybox.Volume();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
