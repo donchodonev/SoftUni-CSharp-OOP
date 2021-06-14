da995f307a24f0400731ea68fcaee03f76dbe091
donchodonev <dummyacc9292@gmail.com>
2021-06-14 12:06:54 +0300

    added another exercise solution

---
+++ 1.Inheritance/2.Exercise/Restaurant/Cake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        public Cake(string name)
+             : base(name, CakePrice, Grams, Calories)
+        {
+
+        }
+        private const double Grams = 250.0;
+        private const double Calories = 1000.0;
+        private const decimal CakePrice = 5M;
+    }
+}
---
+++ 1.Inheritance/2.Exercise/Restaurant/Food.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Food : Product
+    {
+        public Food(string name, decimal price, double grams)
+            :base(name, price)
+        {
+            Name = name;
+            Price = price;
+            Grams = grams;
+        }
+        public string  Name { get; set; }
+        public decimal Price { get; set; }
+        public double Grams { get; set; }
+    }
+}
