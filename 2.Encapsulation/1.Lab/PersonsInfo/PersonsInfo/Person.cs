9c8b4bc06e9c84e3fbb56952480874600831eb4c
donchodonev <dummyacc9292@gmail.com>
2021-06-14 17:12:23 +0300

    updated exercise solution to meet requirements of next task by adding data validation

--- 2.Encapsulation/1.Lab/PersonsInfo/PersonsInfo/Person.cs
+++ 2.Encapsulation/1.Lab/PersonsInfo/PersonsInfo/Person.cs
@@ -14,10 +14,61 @@ namespace PersonsInfo
             Salary = salary;
         }
 
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
-        public int Age { get; private set; }
-        public decimal Salary { get; private set; }
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get => firstName;
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                lastName = value;
+            }
+        }
+        public int Age
+        {
+            get => age;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                age = value;
+            }
+        }
+        public decimal Salary
+        {
+            get => salary;
+            set
+            {
+                if (value < 460)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+                }
+                salary = value;
+            }
+        }
+
         public override string ToString()
         {
             return $"{FirstName} {LastName} receives {Salary:F2} leva.";
