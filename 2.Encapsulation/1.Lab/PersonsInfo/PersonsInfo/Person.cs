3f02009c5da3cf2f1fe08df12d0fc274be848426
donchodonev <dummyacc9292@gmail.com>
2021-06-14 17:00:41 +0300

    updated solution to meet criteria for 2nd exercise condition

--- 2.Encapsulation/1.Lab/PersonsInfo/PersonsInfo/Person.cs
+++ 2.Encapsulation/1.Lab/PersonsInfo/PersonsInfo/Person.cs
@@ -6,19 +6,30 @@ namespace PersonsInfo
 {
     public class Person
     {
-        public Person(string firstName, string lastName, int age)
+        public Person(string firstName, string lastName, int age, decimal salary)
         {
             FirstName = firstName;
             LastName = lastName;
             Age = age;
+            Salary = salary;
         }
 
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
         public int Age { get; private set; }
+        public decimal Salary { get; private set; }
         public override string ToString()
         {
-            return $"{FirstName} {LastName} is {Age} years old.";
+            return $"{FirstName} {LastName} receives {Salary:F2} leva.";
+        }
+        public void IncreaseSalary(decimal parcentage)
+        {
+            if (Age < 30)
+            {
+                parcentage /= 2;
+            }
+
+            Salary = Salary + ((Salary / 100M) * parcentage);
         }
     }
 }
