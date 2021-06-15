51bfa156796c57eb8db1ad0e1c2d6dd008fdf094
donchodonev <dummyacc9292@gmail.com>
2021-06-15 17:15:33 +0300

    added another exercise solution

---
+++ 2.Encapsulation/2.Exercise/ShoppingSpree/ShoppingSpree/Person.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private const decimal MinMoney = decimal.Zero;
+        private decimal money;
+        private string name;
+        private readonly List<Product> bag = new List<Product>();
+        public Person(string name, decimal money)
+        {
+            Money = money;
+            Name = name;
+        }
+        private decimal Money
+        {
+            get { return money; }
+            set
+            {
+                if (value < MinMoney)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                money = value;
+            }
+        }
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                name = value;
+            }
+        }
+        public IReadOnlyList<Product> Products
+        {
+            get => bag.AsReadOnly();
+        }
+
+        public void Add(Product product)
+        {
+            if (product.Cost > Money)
+            {
+                throw new ArgumentException($"{Name} can't afford {product.Name}");
+            }
+            Console.WriteLine($"{Name} bought {product.Name}");
+            bag.Add(product);
+            Money -= product.Cost;
+        }
+        public override string ToString()
+        {
+            if (bag.Count == 0)
+            {
+                return $"{Name} - Nothing bought";
+            }
+
+            return $"{Name} - {string.Join(", ", bag.Select(p => p.Name))}";
+        }
+    }
+}
---
+++ 2.Encapsulation/2.Exercise/ShoppingSpree/ShoppingSpree/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingSpree
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //fk this exercise
+
+            List<Person> persons = new List<Person>();
+            List<Product> products = new List<Product>();
+
+            try
+            {
+                persons = Console.ReadLine()
+                    .Split(';')
+                    .ToList()
+                    .Select(t => t.Split('='))
+                    .Select(p => new Person(p[0], decimal.Parse(p[1])))
+                    .ToList();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            try
+            {
+                products = Console.ReadLine()
+                    .Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToList()
+                    .Select(t => t.Split('='))
+                    .Select(p => new Product(p[0], decimal.Parse(p[1])))
+                    .ToList();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            string cmdArgs = Console.ReadLine();
+
+            while (cmdArgs != "END")
+            {
+                string[] tokens = cmdArgs.Split();
+                var person = persons.First(p => p.Name == tokens[0]);
+                var product = products.First(p => p.Name == tokens[1]);
+
+                try
+                {
+                    person.Add(product);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                cmdArgs = Console.ReadLine();
+            }
+
+            //print
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
