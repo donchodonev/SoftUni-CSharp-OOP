a1f7bb76fd452727a4d73deffa116a521be2975b
donchodonev <dummyacc9292@gmail.com>
2021-06-17 12:21:00 +0300

    added another exercise solution

---
+++ 3.Interfaces and Abstraction/2.Exercise/Exercises/BorderControl/Citizen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    public class Citizen : IIdentifiable
+    {
+        public Citizen(string name, int age, string iD)
+        {
+            Age = age;
+            Name = name;
+            ID = iD;
+        }
+
+        public int Age { get; private set; }
+        public string Name { get; private set; }
+        public string ID { get; set; }
+    }
+}
---
+++ 3.Interfaces and Abstraction/2.Exercise/Exercises/BorderControl/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BorderControl
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<IIdentifiable> robotsAndCitizens = new List<IIdentifiable>();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] inputArgs = input
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (inputArgs.Length == 3)
+                {
+                    string name = inputArgs[0];
+                    int age = int.Parse(inputArgs[1]);
+                    string id = inputArgs[2];
+
+                    robotsAndCitizens.Add(new Citizen(name, age, id));
+                }
+                else
+                {
+                    string model = inputArgs[0];
+                    string id = inputArgs[1];
+
+                    robotsAndCitizens.Add(new Robot(model, id));
+                }
+
+                input = Console.ReadLine();
+
+            }
+
+            string idEnding = Console.ReadLine();
+
+            foreach (var robotsAndCitizen in robotsAndCitizens
+                .Where(x => x.ID.EndsWith(idEnding)))
+            {
+                Console.WriteLine(robotsAndCitizen.ID);
+            }
+        }
+    }
+}
