b5e1cf55e16e2b3b25cf6e2c7c78ee7524adc2cf
donchodonev <dummyacc9292@gmail.com>
2021-06-17 09:47:43 +0300

    added final exercise solution

---
+++ 3.Interfaces and Abstraction/1.Lab/Shapes/Cars/ICar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    interface ICar
+    {
+        public string Color { get; set; }
+        public string Model { get; set; }
+        public string Stop();
+        public string Start();
+    }
+}
