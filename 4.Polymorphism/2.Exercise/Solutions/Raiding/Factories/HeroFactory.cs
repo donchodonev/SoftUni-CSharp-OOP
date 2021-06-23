d39b6a40894bd40af7caea0539fd76c21ea85468
donchodonev <dummyacc9292@gmail.com>
2021-06-23 17:13:31 +0300

    tiny format change

--- 4.Polymorphism/2.Exercise/Solutions/Raiding/Factories/HeroFactory.cs
+++ 4.Polymorphism/2.Exercise/Solutions/Raiding/Factories/HeroFactory.cs
@@ -7,10 +7,12 @@ namespace Raiding.Models
     public abstract class HeroFactory
     {
         protected string heroName = default;
+
         protected HeroFactory(string name)
         {
             heroName = name;
         }
+
         public abstract BaseHero CreateHero();
     }
 }
