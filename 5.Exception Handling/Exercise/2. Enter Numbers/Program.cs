3a2e668fced690e62e697fef80a741e2c9e52e89
donchodonev <dummyacc9292@gmail.com>
2021-06-24 10:48:19 +0300

    added another exercise solution

---
+++ 5.Exception Handling/Exercise/2. Enter Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _2._Enter_Numbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReadNumber(int.Parse(Console.ReadLine()));
+        }
+
+        public static void ReadNumber(int number)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    int n = number;
+
+                    if (number < 1 || number > 100)
+                    {
+                        throw new ArgumentException();
+                    }
+                    number = int.Parse(Console.ReadLine());
+                }
+                catch (ArgumentException ex)
+                {
+                    i = 0;
+                    number = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+    }
+}
