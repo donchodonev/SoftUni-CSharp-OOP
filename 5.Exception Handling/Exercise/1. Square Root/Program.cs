db8652b7b0146cfd3adbefe7d0c33099aa4473ef
donchodonev <dummyacc9292@gmail.com>
2021-06-24 10:48:05 +0300

    added first exercise solution

---
+++ 5.Exception Handling/Exercise/1. Square Root/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1._Square_Root
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine(Math.Sqrt(n));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
