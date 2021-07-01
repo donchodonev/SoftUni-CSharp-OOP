f7ac824e45560a5874057f85f33bc486d9b6fb0c
donchodonev <dummyacc9292@gmail.com>
2021-07-01 17:23:41 +0300

    updated solution to solve 2nd exercise task

--- 7.Reflection and Attributes/Stealer/Spy.cs
+++ 7.Reflection and Attributes/Stealer/Spy.cs
@@ -16,7 +16,7 @@ namespace Stealer
             var hackerInstance = Activator.CreateInstance(hacker, null);
 
             var fields = hacker
-                .GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+                .GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
 
             sb.AppendLine($"Class under investigation: {nameOfClass}");
 
@@ -26,6 +26,39 @@ namespace Stealer
                 {
                     sb.AppendLine($"{field.Name} = {field.GetValue(hackerInstance)}");
                 }
+            }
+
+            return sb.ToString();
+        }
+
+        public string AnalyzeAcessModifiers(string className)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var objectType = Type.GetType(className);
+
+            var hackerInstance = Activator.CreateInstance(objectType, null);
+
+            var fields = objectType
+                .GetFields(BindingFlags.Instance | BindingFlags.Public);
+
+            var publicSetters = objectType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+
+            var privateGetters = objectType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            foreach (var fieldInfo in fields)
+            {
+                sb.AppendLine($"{fieldInfo.Name} must be private!");
+            }
+
+            foreach (var setter in privateGetters.Where(s => s.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{setter.Name} have to be public!");
+            }
+
+            foreach (var setter in publicSetters.Where(s => s.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{setter.Name} have to be private!");
             }
 
             return sb.ToString();
--- 7.Reflection and Attributes/Stealer/StartUp.cs
+++ 7.Reflection and Attributes/Stealer/StartUp.cs
@@ -7,7 +7,8 @@ namespace Stealer
         public static void Main(string[] args)
         {
             Spy spy = new Spy();
-            string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            //string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            string result = spy.AnalyzeAcessModifiers("Stealer.Hacker");
             Console.WriteLine(result);
         }
     }
