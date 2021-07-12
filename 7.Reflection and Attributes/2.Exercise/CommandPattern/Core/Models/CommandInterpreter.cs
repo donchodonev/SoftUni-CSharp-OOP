d215fe78087d8a883ccf507e37101764935368c3
donchodonev <dummyacc9292@gmail.com>
2021-07-12 12:12:24 +0300

    added reflection substituted if-else construction in the CommandInterpreter class with reflection

--- 7.Reflection and Attributes/2.Exercise/CommandPattern/Core/Models/CommandInterpreter.cs
+++ 7.Reflection and Attributes/2.Exercise/CommandPattern/Core/Models/CommandInterpreter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using CommandPattern.Core.Contracts;
 
@@ -8,26 +9,41 @@ namespace CommandPattern.Core.Models
 {
     class CommandInterpreter : ICommandInterpreter
     {
+        private const string commandNameEnding = "Command";
         private ICommand commandToExecute;
         private string result = string.Empty;
         private string commandName;
         public string Read(string args)
         {
-            commandName = args.Split()[0];
+            commandName = args.Split()[0] + commandNameEnding;
 
-            if (commandName == "Hello")
+            var commandType = Assembly.GetCallingAssembly()
+                .GetTypes()
+                .Where
+                (t => t.Name == commandName && t.GetInterfaces()
+                    .Any(i => i.Name.EndsWith(commandNameEnding))
+                );
+
+            if (commandType.FirstOrDefault() == null)
             {
-                commandToExecute = new HelloCommand();
+                throw new InvalidOperationException("Type not found");
             }
-            else if (commandName == "Exit")
+
+            try
             {
-                commandToExecute = new ExitCommand();
+                commandToExecute = Activator.CreateInstance(commandType.First()) as ICommand;
+            }
+            catch (InvalidCastException ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
             }
 
             string[] cleanData = args
                 .Split()
                 .Skip(1)
                 .ToArray();
+
             result = commandToExecute.Execute(cleanData);
 
             return result;
