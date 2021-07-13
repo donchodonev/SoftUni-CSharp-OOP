3e766f20b628150c1e8cfa6ccdf15382f74161e7
donchodonev <dummyacc9292@gmail.com>
2021-07-13 20:52:29 +0300

    added first two tests

---
+++ 8.Unit Testing/1.Lab/Skeleton.Tests/AxeTests.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+
+[TestFixture]
+public class AxeTests
+{
+    private Axe axe;
+    private Dummy dummy;
+    private Hero hero;
+
+    [SetUp]
+    public void SetUp()
+    {
+        axe = new Axe(10, 10);
+        dummy = new Dummy(10, 10);
+        hero = new Hero("Doncho");
+    }
+
+    [Test]
+    public void AxesLosesDurabilityAfterAttack()
+    {
+        axe.Attack(dummy);
+        Assert.That(axe.DurabilityPoints,Is.EqualTo(9),"Axe durability doesn't change after attack.");
+    }
+
+    [Test]
+    public void Axe_FailsToAttack_WhenBroken()
+    {
+        axe = new Axe(10, 0);
+        Assert.That(() => axe.Attack(dummy),Throws.InvalidOperationException);
+    }
+}
