12e6d5ca70866570bec02216390ed70815dfe7ec
donchodonev <dummyacc9292@gmail.com>
2021-07-14 10:43:47 +0300

    refactored tests by removing magic numbers

--- 8.Unit Testing/1.Lab/Skeleton.Tests/DummyTests.cs
+++ 8.Unit Testing/1.Lab/Skeleton.Tests/DummyTests.cs
@@ -9,46 +9,70 @@ public class DummyTests
     [SetUp]
     public void SetUp()
     {
-        axe = new Axe(10, 10);
+        int axeAttack = 10;
+        int axeDurability = 10;
+
+        axe = new Axe(axeAttack, axeDurability);
     }
 
     [Test]
     public void AfterAttack_DummyLosesHealth()
     {
-        dummy = new Dummy(20, 10);
+        int initialDummyHealth = 20;
+        int initialDummyExperience = 10;
+
+        int healthLeftAfterAttack = 10;
+
+        dummy = new Dummy(initialDummyHealth, initialDummyExperience);
         axe.Attack(dummy);
-        Assert.That(dummy.Health,Is.EqualTo(10));
+        Assert.That(dummy.Health,Is.EqualTo(healthLeftAfterAttack));
     }
 
     [Test]
     public void DeadDummy_ThrowsException_If_Attacked()
     {
-        dummy = new Dummy(0, 10);
-        Assert.That(() => dummy.TakeAttack(10),Throws.InvalidOperationException);
+        int initialDummyHealth = 0;
+        int initialDummyExperience = 10;
+
+        int attackDamage = 10;
+
+        dummy = new Dummy(initialDummyHealth, initialDummyExperience);
+        Assert.That(() => dummy.TakeAttack(attackDamage),Throws.InvalidOperationException);
     }
 
     [Test]
     public void DeadDummy_CanGive_Experience()
     {
-        dummy = new Dummy(0, 10);
+        int initialDummyHealth = 0;
+        int initialDummyExperience = 10;
+
+        int expectedExperienceReturn = 10;
+
+        dummy = new Dummy(initialDummyHealth, initialDummyExperience);
 
         Assert.That(() => dummy.IsDead(), Is.True);
-        Assert.That(() => dummy.GiveExperience(), Is.EqualTo(10));
+        Assert.That(() => dummy.GiveExperience(), Is.EqualTo(expectedExperienceReturn));
     }
 
     [Test]
     public void AliveDummy_Cannot_GiveExperience()
     {
-        dummy = new Dummy(10, 10);
+        int initialDummyHealth = 10;
+        int initialDummyExperience = 10;
+
+        int expectedExperience = 10;
+        string heroName = "Doncho";
+
+        dummy = new Dummy(initialDummyHealth, initialDummyExperience);
 
         Assert.That(() => dummy.IsDead(), Is.False);
 
-        Hero hero = new Hero("Doncho");
+        Hero hero = new Hero(heroName);
 
         hero.Attack(dummy);
 
         Assert.That(() => dummy.IsDead(), Is.True);
 
-        Assert.That(hero.Experience, Is.EqualTo(10));
+        Assert.That(hero.Experience, Is.EqualTo(expectedExperience));
     }
 }
