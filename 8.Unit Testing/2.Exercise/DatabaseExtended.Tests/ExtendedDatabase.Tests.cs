b010ad595a6c2e276e6480ec9d1b8752cf7b8cc8
donchodonev <dummyacc9292@gmail.com>
2021-07-15 09:22:07 +0300

    added test for Add() method

--- 8.Unit Testing/2.Exercise/DatabaseExtended.Tests/ExtendedDatabase.Tests.cs
+++ 8.Unit Testing/2.Exercise/DatabaseExtended.Tests/ExtendedDatabase.Tests.cs
@@ -1,18 +1,40 @@
+using ExtendedDatabaseProject;
 using NUnit.Framework;
 
 namespace Tests
 {
     public class ExtendedDatabaseTests
     {
+        private ExtendedDatabase exDatabase;
+        private Person person;
+        private Person[] personArray;
+
         [SetUp]
         public void Setup()
         {
+            //Arrange
+            long id = 1;
+            string name = "Doncho";
+
+            person = new Person(id, name);
+
+            personArray = new Person[]
+            {
+                new Person(2, "Goshko"),
+                new Person(3, "Dimitrichko")
+            };
+
+            exDatabase = new ExtendedDatabase();
         }
 
         [Test]
-        public void Test1()
+        public void Should_ThrowException_IfAdding_UserWithExisting_Username()
         {
-            Assert.Pass();
+            //Act
+            exDatabase.Add(person);
+            
+            //Assert
+            Assert.That(() => exDatabase.Add(person),Throws.InvalidOperationException);
         }
     }
 }
