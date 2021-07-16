9e388996e72102c975d3ee6dbfa1356470967686
donchodonev <dummyacc9292@gmail.com>
2021-07-16 18:13:33 +0300

    added SetUp for Arena tests added constructor test

--- 8.Unit Testing/2.Exercise/FightingArena.Tests/ArenaTests.cs
+++ 8.Unit Testing/2.Exercise/FightingArena.Tests/ArenaTests.cs
@@ -1,18 +1,40 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Linq;
+using FightingArena;
+using NUnit.Framework;
 
 namespace Tests
 {
     public class ArenaTests
     {
+        private Arena arena;
+        private Warrior attacker;
+        private Warrior defender;
+        private int damage;
+        private int health;
         [SetUp]
         public void Setup()
         {
+            damage = 20;
+            health = 50;
+
+            arena = new Arena();
+
+            attacker = new Warrior("Attacker", damage, health);
+            defender = new Warrior("Defender", damage, health);
         }
 
+        //Constructor test
+
         [Test]
-        public void Test1()
+        public void EmptyConstructor_Initialises_EmptyWarriorList()
         {
-            Assert.Pass();
+            //Arrange
+            List<Warrior> expectedResult = new List<Warrior>();
+            List<Warrior> actualResult = arena.Warriors.ToList();
+
+            //Assert
+            Assert.AreEqual(expectedResult,actualResult);
         }
     }
 }
