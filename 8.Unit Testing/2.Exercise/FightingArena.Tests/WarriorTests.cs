ea037a52d13060c1db046a36cd341b78e1f460b0
donchodonev <dummyacc9292@gmail.com>
2021-07-16 16:34:49 +0300

    added costructor, setters and getters tests

--- 8.Unit Testing/2.Exercise/FightingArena.Tests/WarriorTests.cs
+++ 8.Unit Testing/2.Exercise/FightingArena.Tests/WarriorTests.cs
@@ -1,18 +1,79 @@
+using System;
+using FightingArena;
 using NUnit.Framework;
 
 namespace Tests
 {
     public class WarriorTests
     {
+        private string name;
+        private int damage;
+        private int hp;
+        private Warrior warrior;
+        private string[] whitespacesEmptyAndNull;
+
         [SetUp]
         public void Setup()
         {
+            //Arrange
+
+            name = "Doncho";
+            damage = 10;
+            hp = 10;
+
+            warrior = new Warrior(name, damage, hp);
+        }
+
+        //Constructor and getters test
+
+        [Test]
+        public void Constructor_Should_AssignValuesCorrectly_To_BackingField()
+        {
+            //Assert
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(this.name, warrior.Name);
+                Assert.AreEqual(this.damage, warrior.Damage);
+                Assert.AreEqual(this.hp, warrior.HP);
+            });
+        }
+
+        //Setters tests
+
+        [Test]
+        public void NameSetter_Should_ThrowException_When_NullOrWhitespace_IsPassed()
+        {
+            //Arrange
+            whitespacesEmptyAndNull = new[] {"", "\n", "\r", "\t", "\f", "\v", null};
+
+            //Assert
+            Assert.Multiple(() =>
+            {
+                Assert.Throws<ArgumentException>(() => new Warrior(whitespacesEmptyAndNull[0], this.damage, this.hp));
+                Assert.Throws<ArgumentException>(() => new Warrior(whitespacesEmptyAndNull[1], this.damage, this.hp));
+                Assert.Throws<ArgumentException>(() => new Warrior(whitespacesEmptyAndNull[2], this.damage, this.hp));
+                Assert.Throws<ArgumentException>(() => new Warrior(whitespacesEmptyAndNull[3], this.damage, this.hp));
+                Assert.Throws<ArgumentException>(() => new Warrior(whitespacesEmptyAndNull[4], this.damage, this.hp));
+                Assert.Throws<ArgumentException>(() => new Warrior(whitespacesEmptyAndNull[5], this.damage, this.hp));
+                Assert.Throws<ArgumentException>(() => new Warrior(whitespacesEmptyAndNull[6], this.damage, this.hp));
+            });
+        }
+
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void DamageSetter_Should_ThrowException_When_Damage_IsLessOrEqualTo_0(int damage)
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior(this.name, damage, this.hp));
         }
 
         [Test]
-        public void Test1()
+        public void HP_Setter_Should_ThrowException_When_Damage_IsLessOrEqualTo_0()
         {
-            Assert.Pass();
+            //Arrange
+            int hp = -1;
+
+            //Assert
+            Assert.Throws<ArgumentException>(() => new Warrior(this.name, this.damage, hp));
         }
     }
 }
