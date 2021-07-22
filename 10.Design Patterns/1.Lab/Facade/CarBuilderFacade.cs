6df1376851978437993ddf967d956b0f19dcf1cb
donchodonev <dummyacc9292@gmail.com>
2021-07-22 10:10:17 +0300

    implemented Facade pattern
    A good explanation for it would be - https://www.dofactory.com/net/facade-design-pattern

---
+++ 10.Design Patterns/1.Lab/Facade/CarBuilderFacade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facade
+{
+    public class CarBuilderFacade
+    {
+        public CarBuilderFacade()
+        {
+            Car = new Car();
+        }
+
+        public CarInfoBuilder Info => new CarInfoBuilder(Car);
+        public CarAddressBuilder Built => new CarAddressBuilder(Car);
+        protected Car Car { get; set; }
+
+        public Car Build() => Car;
+    }
+}
---
+++ 10.Design Patterns/1.Lab/Facade/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Facade
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            CarBuilderFacade carBuilder = new CarBuilderFacade()
+                .Info
+                .WithType("Audi")
+                .WithColor("Black")
+                .WithNumberOfDoors(2)
+                .Built
+                .InCity("Bavaria")
+                .AtAddress("Ingolstadt");
+
+            Car car = carBuilder.Build();
+
+            Console.WriteLine(car);
+        }
+    }
+}
