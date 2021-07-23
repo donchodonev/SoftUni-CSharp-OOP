c0afcd661c9ece06e412144e3282d0546fc30e46
donchodonev <dummyacc9292@gmail.com>
2021-07-23 12:06:02 +0300

    restructured projects in solution

--- 10.Design Patterns/2.Exercise/Prototype Pattern/Program.cs
+++ 10.Design Patterns/2.Exercise/Prototype Pattern/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Prototype_Pattern.ConcretePrototype;
+using SandwichMenu;
 
 namespace Prototype_Pattern
 {
@@ -6,7 +7,13 @@ namespace Prototype_Pattern
     {
         static void Main(string[] args)
         {
+            Menu sandwichMenu = new Menu();
 
+            sandwichMenu["Mazniya sandvich"] = new Sandwich("White", "Chedder", "Pork", "Sivriya pepper");
+            sandwichMenu["Ani sandvich"] = new Sandwich("Wholegrain", "Blue cheese", "", "Cucumber");
+
+            Sandwich mySandwich = sandwichMenu["Mazniya sandvich"].Clone() as Sandwich;
+            Sandwich naAnitoSandvicha = sandwichMenu["Ani sandvich"].Clone() as Sandwich;
         }
     }
 }
